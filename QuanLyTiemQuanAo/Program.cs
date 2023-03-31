a1339ecd3b9e1a6e3747310922ba258c9d6b1270
hoangdang <daohoangdang1123@gmail.com>
2023-03-31 10:47:56 +0700

    thêm comment

--- QuanLyTiemQuanAo/Program.cs
+++ QuanLyTiemQuanAo/Program.cs
@@ -14,6 +14,7 @@ namespace QuanLyTiemQuanAo
         [STAThread]
         static void Main()
         {
+            //thêm comment
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
