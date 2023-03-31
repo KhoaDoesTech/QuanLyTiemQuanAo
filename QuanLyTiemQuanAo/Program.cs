fff14ceec0b81069828bbc801539b91db73c04c1
Anh Khoa Đỗ <khoahoc72@gmail.com>
2023-03-31 11:11:34 +0700

    là sao

--- QuanLyTiemQuanAo/Program.cs
+++ QuanLyTiemQuanAo/Program.cs
@@ -11,10 +11,11 @@ namespace QuanLyTiemQuanAo
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        [STAThread]
+        [STAThread] 
         static void Main()
         {
-            //thêm comment
+            
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
