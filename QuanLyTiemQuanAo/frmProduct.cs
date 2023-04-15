2e8ab744c53349f9e56cbafc4c5090a466d7d298
Anh Khoa Đỗ <khoahoc72@gmail.com>
2023-04-15 19:07:21 +0700

    Product sắp xong

--- QuanLyTiemQuanAo/frmProduct.cs
+++ QuanLyTiemQuanAo/frmProduct.cs
@@ -105,15 +105,5 @@ namespace QuanLyTiemQuanAo
         {
             MoHienThi();
         }
-
-        private void cb_product_type_id_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
     }
 }
---
+++ QuanLyTiemQuanAo/frmProductOld.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using BALayer;
+
+namespace QuanLyTiemQuanAo
+{
+    public partial class frmProductOld : Form
+    {
+        DB_Product dbProduct;        
+        DataTable dtProduct = null;
+        
+        public frmProductOld()
+        {
+            InitializeComponent();
+            dbProduct = new DB_Product();
+        }
+
+        void LoadData()
+        {
+            try
+            {
+                dtProduct = new DataTable();
+                dtProduct.Clear();
+                dtProduct = dbProduct.GetProduct();
+
+                dgvPRODUCT.DataSource = dtProduct;
+            }
+            catch (SqlException e)
+            {
+                MessageBox.Show("Không lấy được nội dung trong table KHACHHANG. Lỗi rồi!!!" + e.Message);
+            }
+        }
+
+        private void FrmKhachHang_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void dgvPRODUCT_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
